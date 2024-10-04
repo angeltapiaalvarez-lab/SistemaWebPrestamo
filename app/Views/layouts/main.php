96c77518a11d32579d80752523ee978f12be1ae3
Angel Enrique Tapia Alvarez <AT8466NI@grupomonge.com>
2024-10-04 11:27:25 -0600

    Se agregan cambios, pantalla de inicio y se crean clases a utilizar para la configuracion de los diferentes modulos

---
+++ app/Views/layouts/main.php
@@ -0,0 +1,23 @@
+<!--head-->
+
+<body>
+  <div class="loader"></div>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+      <!--menus-->
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <!--contenido-->
+        </section>
+        <!--paint-->
+      </div>
+      <!--footer-->
+    </div>
+  </div>
+
+</body>
+
+</html>
---
+++ app/Views/layouts/styles.php
@@ -0,0 +1,5 @@
+ <!-- General CSS Files -->
+ <link rel="stylesheet" href="assets/css/app.min.css">
+ <!-- Template CSS -->
+ <link rel="stylesheet" href="assets/css/style.css">
+ <link rel="stylesheet" href="assets/css/components.css">
