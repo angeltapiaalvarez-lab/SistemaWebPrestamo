8eb09173d436e3585cb8544cce4874097b51bf56
Angel456-lab <angel.tapia78910@gmail.com>
2025-08-27 22:17:28 -0600

    se agrega leyenda /Cedula

--- app/Views/prestamos/nuevo.php
+++ app/Views/prestamos/nuevo.php
@@ -21,7 +21,7 @@ Nuevo prestamo
                     <div class="form-group">
                         <label>Buscar cliente</label>
                         <input type="hidden" id="id_cliente" name="id_cliente" value="<?php echo set_value('id_cliente'); ?>">
-                        <input type="text" id="cliente" name="cliente" class="form-control" value="<?php echo set_value('cliente'); ?>" placeholder="Nombre">
+                        <input type="text" id="cliente" name="cliente" class="form-control" value="<?php echo set_value('cliente'); ?>" placeholder="Nombre/Cédula">
                         <span class="text-danger" id="errorCliente"></span>
                         <?php if (!empty($errors['id_cliente']) || !empty($errors['cliente'])) { ?>
                             <span class="text-danger"><?php echo $errors['cliente']; ?></span>
